c2522b2dd8224e6c48d586133c77d58de68115ee
Danilo Canguçu <daniloccangucu@gmail.com>
2025-11-30 15:14:36 +0100

    Refactor NavBar styles and structure
    Updated NavBar component to use new CSS classes for improved layout and styling. Added 'ul', 'nav-signup', and 'text-decoration-none' classes to navbar.module.css and applied them in NavBar.tsx for better spacing and text decoration control.

--- src/app/components/shared/NavBar.tsx
+++ src/app/components/shared/NavBar.tsx
@@ -5,13 +5,13 @@ export function NavBar({ isSignUpPage }: { isSignUpPage: boolean }) {
   return (
     <header className={styles.navbar}>
       <nav className="u-container">
-        <ul>
-        <li className={styles['nav-logo']}>
-          <Link className={styles.a} href="/">Ziriguidum</Link>
+        <ul className={styles.ul}>
+          <li className={styles['nav-logo']}>
+            <Link className={styles['text-decoration-none']} href="/">Ziriguidum</Link>
         </li>
         {!isSignUpPage && (
-        <li>
-          <Link href="/sign-up">Sign Up</Link>
+            <li className={styles['nav-signup']}>
+              <Link href="/sign-up">Sign up</Link>
         </li>
         )}
         </ul>
