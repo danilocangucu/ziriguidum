0db6e8509b0d1f7c5a392058f1fb32e5f4856eff
Danilo Canguçu <daniloccangucu@gmail.com>
2025-12-01 13:51:49 +0100

    Add HowItWorks component for course details
    Introduces the HowItWorks React component, outlining the structure and schedule of the mini-course, including session topics, group sizes, and meeting times for both online and Berlin groups.

---
+++ src/app/components/Home/HowItWorks.tsx
@@ -0,0 +1,57 @@
+export default function HowItWorks() {
+  return (
+    <section className="bg-color-secondary padding-bottom-s">
+      <section className="u-container">
+        <h2>How the mini-course works</h2>
+        <article>
+          <p>
+            During our ziriguidum meetings, we will play songs, read lyrics with
+            English translations, and sometimes watch music videos or live
+            performances. I also share historical context, artistic background,
+            and little anecdotes along the way, making each session engaging and
+            immersive.
+          </p>
+          <p>
+            This time, <strong>in February 2026,</strong> I’m hosting two
+            evening groups:{" "}
+            <strong>
+              one online (CET, Central European Time) on Mondays, and another
+              one at my home in Berlin (Neukölln), on Tuesdays
+            </strong>
+            .
+          </p>
+          <ul>
+            <li>
+              The online group will start on Monday, February 2nd, at 18.30, and
+              its last meeting will be on February 23rd, same time.
+            </li>
+            <li>
+              The Berlin group will start on Tuesday, February 3rd, at 18.30,
+              and its last meeting will be on February 24th, same time.
+            </li>
+          </ul>
+          <p>
+            To keep our sessions personal and interactive,{" "}
+            <strong>each group will have no more than 10 participants.</strong>
+          </p>
+          <p>
+            The mini-course is divided into{" "}
+            <strong>four one-hour sessions</strong> (sometimes a few minutes
+            longer), each dedicated to a single genre. The first three cover
+            samba, bossa nova, and forró, while the final session brings
+            together a variety of rhythms such as MPB, Brazilian rock, pop, and
+            more.
+          </p>
+          <p className="margin-bottom-0 padding-bottom-2xs">
+            By the end of the course, you’ll have a solid foundation; enough to
+            keep exploring Brazilian music on your own.{" "}
+            <strong>
+              Each session also comes with a curated playlist to accompany what
+              we discuss.
+            </strong>
+          </p>
+        </article>
+      </section>
+    </section>
+  );
+}
